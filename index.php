d471328f62079901af2940b8c458d70c214d40f2
Dominik Wajda <admin@epicsite.xyz>
2022-12-29 19:02:05 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,192 @@
+<html>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta property="og:title" content="What could possibly be on this website that is so Epic?">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://epicsite.xyz">
+        <meta property="og:description" content="What could possibly be on this website that is so Epic?">
+        <meta name="theme-color" content="#303EE1">
+        <meta name="author" content="My Epic Site">
+         
+         <meta name="description" content="What could possibly be on this website that is so Epic?">
+         <meta name="keywords" content="epicsite, Epic, epic, youtube">
+         <meta property="og:locale" content="en_US">
+
+         <meta property="og:type" content="video.other">
+         <meta property="og:video:url" content="https://crewmate1.netlify.app/roll.mp4">
+         <meta property="og:video:secure_url" content="https://crewmate1.netlify.app/roll.mp4">
+         <meta property="og:video:type" content="text/html">
+         <meta property="og:video:width" content="1280">
+         <meta property="og:video:height" content="720">
+
+         <span><meta property="og:site_name" content="My Epic Site">
+         <link itemprop="name" content="My Epic Site"></span>
+           
+
+<head>
+<body>
+    <div onmousemove="playVideo()">
+    <div onload="playVideo()">
+<script>
+window.onbeforeunload = function (e) {
+    e = e || window.event;
+
+    // For IE and Firefox prior to version 4
+    if (e) {
+        e.returnValue = 'Sure?';
+    }
+
+    // For Safari
+    return 'Sure?';
+};
+</script>
+  <h1> Play the video i dare you. </h1>
+  <title>My Epic Site</title>
+	<video id="rr" style="border-radius:12px; width:100%; max-width:852;" autoplay loop controls>
+      <source src="https://crewmate1.netlify.app/roll.mp4" type="video/mp4"> Your Browser Sucks! Can't play the video.
+	   </video>
+	   <script> 
+var myVideo = document.getElementById("rr");
+function playVideo() { 
+ myVideo.play(); 
+} 
+function pauseVideo() { 
+ myVideo.pause(); 
+} 
+</script> 
+  <h6></h6>
+	   <style>
+      .button {
+        display: inline-block;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        background-color: rgba(20,20,20,0.2);
+        border-radius: 6px;
+        outline: none;
+        transition: transform 0.1s;
+        transition: 0.3s
+      }
+      .button:hover {
+        background-color: rgba(100,100,100,0.2);
+        transition: transform 0.2s;
+        transform: scale(1.1);
+      }
+      .special {
+          background-color: rgba(100,20,100,0.5);
+          color: white;
+      }
+      .special:hover {
+          background-color: rgba(100,20,100,0.5);
+          color: white;
+}
+        .special-r {
+          background-color: rgba(100,20,20,0.5);
+          color: white;
+      }
+      .special-r:hover {
+          background-color: rgba(100,20,20,0.5);
+          color: white;
+      }
+          .special-go {
+          background-color: rgba(100,100,20,0.5);
+          color: white;
+      }
+      .special-go:hover {
+          background-color: rgba(100,100,20,0.5);
+          color: white;
+      }
+          .special-wh {
+          background-color: rgba(100,100,100,0.5);
+          color: white;
+      }
+      .special-wh:hover {
+          background-color: rgba(100,100,100,0.5);
+          color: white;
+      }
+          .special-gr {
+          background-color: rgba(20,100,20,0.5);
+          color: white;
+      }
+      .special-gr:hover {
+          background-color: rgba(20,100,20,0.5);
+          color: white;
+      }
+    </style>	
+    <a class="button" href="/donate">Donate</a>
+    <a class="button" href="/sus.mp4">Watch sus.mp4</a>
+    <a class="button" href="/sus1.mp4">Watch sus1.mp4</a>
+    <a class="button" href="/projects">My Projects</a>
+    <a class="button" href="/contact">Contact Me</a>
+    <a class="button" href="https://bunkercoin.xyz"><img src="bkc.webp" style="width:10;">Get Bunkercoin!</a>
+	<br><img src="tr.png" style="width:9;"><br>
+	<a class="button" href="/yourip.html">I have your IP</a>
+    <a class="button" onclick="Alert.render('ok')" class="btn">Free $5</a>
+    <a class="button" href="https://sysdl.epicsite.xyz">sysdl</a>
+    <a class="button" href="/cookieclicker">Cookie Clicker unblocked</a>
+    <a class="button" href="/download">Download</a>
+    <a class="button" href="https://stats.uptimerobot.com/2oDqKuVvoz">Uptime</a>
+    <br><img src="tr.png" style="width:9;"><br>
+    <a class="button special" href="https://badyt.cf">Bad YouTube</a>
+    <a class="button special-go" href="https://badytbeta.epicsite.xyz">Bad YouTube Beta</a>
+    <a class="button special-wh" href="https://invidious.epicsite.xyz">Invidious</a>
+    <a class="button special-r" href="https://nitter.epicsite.xyz">Nitter</a>
+    <a class="button special-gr" href="https://files.epicsite.xyz">Files Repository</a>
+    
+<br>
+<div id="popUpOverlay"></div>
+<div id="popUpBox">
+<div id="box">
+<h1>Get 5$ for free using Honeygain</h1>
+<h3> Using this link to sign up for Honeygain gets you 5$ in your Honeygain balance.
+Click the link below to sign up.</h3>
+<h3> Fun fact: The domain epicsite.xyz was bought entirely using funds from honeygain. </h3>
+<a href="https://r.honeygain.me/DOMINE9D94">the link</a>
+<h6></h6>
+<div id="closeModal"></div>
+</div>
+</div>
+
+<h6></h6>
+<style>
+#popUpBox{
+  width: 300px;
+  overflow: hidden;
+  background: #333;
+  box-shadow: 0 0 20px black;
+  border-radius: 10px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 9999;
+  padding: 10px;
+  text-align: center;
+  display: none;
+}
+</style>
+<script>
+var Alert = new CustomAlert();
+function CustomAlert(){
+  this.render = function(){
+      //Show Modal
+      let popUpBox = document.getElementById('popUpBox');
+      popUpBox.style.display = "block";
+      //Close Modal
+      document.getElementById('closeModal').innerHTML = '<button class="button" onclick="Alert.ok()">Thanks</button>';
+  }
+this.ok = function(){
+  document.getElementById('popUpBox').style.display = "none";
+  document.getElementById('popUpOverlay').style.display = "none";
+}
+
+}		
+</script>
+</div>
+<link rel="stylesheet" href="main.css">
+ </body>
+ </div>
+ </div>
+ </head>
+</html>
