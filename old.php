6ed35a90bc41284240bb12a1fe879fe7da3a006b
Dominik Wajda <admin@epicsite.xyz>
2023-10-18 16:57:22 +0100

    a

---
+++ old.php
@@ -0,0 +1,362 @@
+<html>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta property="og:title" content="What could possibly be on this website that is so Epic?">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://epicsite.xyz">
+        <meta property="og:description" content="What could possibly be on this website that is so Epic?">
+        <meta name="theme-color" content="#303EE1">
+        <meta name="author" content="My Epic Site">
+         
+         <meta name="description" content="What could possibly be on this website that is so Epic?">
+         <meta name="keywords" content="epicsite, Epic, epic, youtube">
+         <meta property="og:locale" content="en_US">
+
+         <meta property="og:type" content="video.other">
+         <meta property="og:video:type" content="text/html">
+         <meta property="og:video:width" content="1280">
+         <meta property="og:video:height" content="720">
+
+         <span><meta property="og:site_name" content="My Epic Site">
+         <link itemprop="name" content="My Epic Site"></span>
+         <title> My Epic Site </title>
+           
+
+<head><body>
+<div id="blob"></div>
+<div id="blur"></div>
+<h1>
+	<div class="container">
+        <a type="button" onclick="showPopup()">Contact</a>
+        <a type="button" onclick="showPopup2()">Services</a>
+        <a type="button" onclick="showPopup3()">Referral Links</a>
+
+<style>
+		.popup, .popup2, .popup3 {
+			display: none;
+			position: fixed;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			z-index: 9999;
+		}
+		.popup-content {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			background-color: rgba(0,0,0,0.9);
+			padding: 20px;
+			border-radius: 5px;
+			box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+		}
+		.popup-close {
+			position: absolute;
+			top: 10px;
+			right: 10px;
+			font-size: 24px;
+			font-weight: bold;
+			cursor: pointer;
+		}
+	</style>
+
+		<div class="popup" id="popup">
+			<div class="popup-content">
+				<span class="popup-close" onclick="hidePopup()">&times;</span>
+				
+
+
+Discord: <a href="//discord.com/users/534997987424993280/">GoldDominik893#0266</a><br>
+Email: <a href="mailto:admin@epicsite.xyz">admin@epicsite.xyz</a><br>
+Matrix: <a href="//matrix.to/#/@golddominik893:matrix.org">@golddominik893:matrix.org</a><br>
+GitHub: <a href="//github.com/golddominik893/">GoldDominik893</a></h1>
+
+			</div>
+		</div>
+<h1>
+        <div class="popup2" id="popup2">
+			<div class="popup-content">
+				<span class="popup-close" onclick="hidePopup()">&times;</span>
+Services:
+<div class="referral-links">
+  <a href="//liberatube.store">
+    <img src="//liberatube.store/favicon.ico" class="referral-image">
+    Liberatube
+  </a>
+  <a href="//boiurl.epicsite.xyz">
+    <img src="https://myket.ir/app-icon/1c35c61c-d0cd-49df-bb12-f68345ca9bc0_.png" class="referral-image">
+    Boiurl
+  </a>
+  <a href="//libreddit.epicsite.xyz">
+    <img src="//libreddit.epicsite.xyz/favicon.ico" alt="Image 3" class="referral-image">
+    Libreddit
+  </a>
+</div>
+
+			</div>
+		</div>
+</h1>
+<h1>
+<div class="popup3" id="popup3">
+			<div class="popup-content">
+				<span class="popup-close" onclick="hidePopup()">&times;</span>
+                Referral Links:
+<div class="referral-links">
+  <a href="//salad.io">
+    <img src="https://styles.redditmedia.com/t5_qwslm/styles/communityIcon_8rhp9a9fce041.png" class="referral-image">
+    Salad: ABOSM2
+  </a>
+  <a href="//r.honeygain.me/DOMINE9D94">
+    <img src="https://i.pinimg.com/originals/76/ca/8e/76ca8ea85b973ad8168da76b5ab4ceb3.png" class="referral-image">
+    Honeygain: DOMINE9D94
+  </a>
+  <a href="#">
+    <img src="" class="referral-image">
+    Empty
+  </a>
+</div>
+
+			</div>
+		</div>
+</h1>
+
+
+	</div>
+
+	<script>
+		function showPopup() {
+			document.getElementById("popup").style.display = "block";
+		}
+
+        function showPopup2() {
+			document.getElementById("popup2").style.display = "block";
+		}
+
+        function showPopup3() {
+			document.getElementById("popup3").style.display = "block";
+		}
+
+		function hidePopup() {
+			document.getElementById("popup").style.display = "none";
+            document.getElementById("popup2").style.display = "none";
+            document.getElementById("popup3").style.display = "none";
+		}
+	</script>
+
+	<style>
+.referral-links {
+  display: flex;
+  flex-direction: column;
+}
+
+.referral-links a {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  text-decoration: none;
+  color: #000;
+}
+
+.referral-image {
+  width: 30px;
+  height: 30px;
+  margin-right: 10px;
+}
+
+.referral-links a:hover {
+  color: #ff0000;
+}
+
+    h1 {
+  font-family: 'Space Mono', monospace;
+  color: white;
+  padding: 0rem clamp(1rem, 2vw, 3rem);
+  border-radius: clamp(0.4rem, 0.75vw, 1rem);
+  margin: 0rem;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  translate: -50% -50%;
+  z-index: 3;
+}
+    .welcome, .services, .contact, .referral-links, .other {
+        background: rgba(20,20,20,0.5);
+        width: 416px;
+        border-radius: 8px;
+    }
+    .inline {
+        display: inline-block;
+        margin-bottom: 5px;
+    }
+      .button {
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        background-color: rgba(20,20,20,0.2);
+        border-radius: 6px;
+        outline: none;
+        transition: transform 0.1s;
+        transition: 0.3s;
+        margin-bottom: 20px;
+      }
+      .button:hover {
+        background-color: rgba(100,100,100,0.2);
+        transition: transform 0.2s;
+        transform: scale(1.1);
+      }
+      @import url('https://fonts.googleapis.com/css2?family=Signika+Negative&display=swap');
+html {
+    scroll-behavior: smooth;
+}
+h1, h2, h3, h4, h5, h6, p {
+    color: white;
+}
+body {
+  background-color: black;
+  height: 100vh;
+  margin: 0rem;
+  overflow: hidden; 
+}
+
+@keyframes rotate {
+  from {
+    rotate: 0deg;
+  }
+  
+  50% {
+    scale: 1 1.5;
+  }
+  
+  to {
+    rotate: 360deg;
+  }
+}
+
+#blob {
+  background-color: white;
+  height: 34vmax;
+  aspect-ratio: 1;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  translate: -50% -50%;
+  border-radius: 50%;
+  background: linear-gradient(to right, aquamarine, mediumpurple);
+  animation: rotate 20s infinite;
+  opacity: 0.8;
+  filter: blur(80px);
+}
+
+#blur {
+  height: 100vh;
+  width: 100vw;
+  position: absolute;
+  z-index: 2;
+  backdrop-filter: blur(12vmax);
+}
+a:visited, a {
+    color: white;
+    text-decoration: none;
+}
+a:hover, a:active {
+    color: cyan;
+    text-decoration: bold;
+}
+.button {
+    color: white;
+}
+::selection {
+    color: cyan;
+    background-color: #00000000;
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 6px;
+}
+/* Track */
+::-webkit-scrollbar-track {
+  background: rgba(255, 0, 0, 0); 
+}
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888; 
+  border-radius: 5px;
+}
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+
+</style>	
+
+<div id="popUpOverlay"></div>
+<div id="popUpBox">
+<div id="box">
+<h1>Get 5$ for free using Honeygain</h1>
+<h3> Using this link to sign up for Honeygain gets you 5$ in your Honeygain balance.
+Click the link below to sign up.</h3>
+<h3> Fun fact: The domain epicsite.xyz was bought entirely using funds from honeygain. </h3>
+<a href="https://r.honeygain.me/DOMINE9D94">the link</a>
+<div id="closeModal"></div>
+</div>
+</div>
+<style>
+#popUpBox{
+  width: 300px;
+  overflow: hidden;
+  background: #333;
+  box-shadow: 0 0 20px black;
+  border-radius: 10px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 9999;
+  padding: 10px;
+  text-align: center;
+  display: none;
+}
+</style>
+<script>
+var Alert = new CustomAlert();
+function CustomAlert(){
+  this.render = function(){
+      //Show Modal
+      let popUpBox = document.getElementById('popUpBox');
+      popUpBox.style.display = "block";
+      //Close Modal
+      document.getElementById('closeModal').innerHTML = '<button class="button" onclick="Alert.ok()">Thanks</button>';
+  }
+this.ok = function(){
+  document.getElementById('popUpBox').style.display = "none";
+  document.getElementById('popUpOverlay').style.display = "none";
+}
+
+}		
+
+/* -- Glow effect -- */
+
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
+}
+</script>
+
+</div>
+<link rel="stylesheet" href="main.css">
+ </body>
+ </head>
+</html>
