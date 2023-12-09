a64a01e76bdd046a66d6733e34c3f00566b7898f
Dominik Wajda <admin@epicsite.xyz>
2023-12-09 10:01:02 +0000

    add audio player

--- js/custom.js
+++ js/custom.js
@@ -48,3 +48,78 @@ window.addEventListener('load', function() {
   });
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+let audioContext;
+    let analyser;
+    let audioSource;
+
+    const canvas = document.getElementById('visualizer');
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = window.innerWidth;
+    canvas.height = 125;
+
+    const bufferLength = 1024; // Adjust this value for better resolution if needed
+    const dataArray = new Uint8Array(bufferLength);
+
+    function setupAudio(audioFile) {
+      audioContext = new (window.AudioContext || window.webkitAudioContext)();
+      analyser = audioContext.createAnalyser();
+
+      audioSource = new Audio();
+      audioSource.src = audioFile;
+      audioSource.crossOrigin = 'anonymous'; // Enable cross-origin for audio
+      audioSource.load();
+      audioSource.play();
+
+      const source = audioContext.createMediaElementSource(audioSource);
+      source.connect(analyser);
+      analyser.connect(audioContext.destination);
+
+      function draw() {
+        requestAnimationFrame(draw);
+
+        analyser.getByteFrequencyData(dataArray);
+
+        ctx.fillStyle = 'rgb(0, 0, 0)';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        const barWidth = (canvas.width / bufferLength) * 2.5;
+        let x = 0;
+
+        for (let i = 0; i < bufferLength; i++) {
+          const barHeight = dataArray[i];
+
+          ctx.fillStyle = `rgb(80, 70, 250)`;
+          ctx.fillRect(x, canvas.height - barHeight / 2, barWidth, barHeight / 2);
+
+          x += barWidth + 1;
+        }
+      }
+
+      audioSource.addEventListener('play', function() {
+        draw();
+      });
+    }
+
+    function playAudio(audioFile) {
+      if (audioContext && audioContext.state === 'suspended') {
+        audioContext.resume().then(function() {
+          setupAudio(audioFile);
+        });
+      } else {
+        setupAudio(audioFile);
+      }
+    }
